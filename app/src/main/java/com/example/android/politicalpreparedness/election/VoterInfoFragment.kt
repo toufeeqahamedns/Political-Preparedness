0fb82084e8b9851a7fe5c33c7ef6a16943219023
toufeeqahamedns <toufeeq.nas@gmail.com>
2021-03-01 22:05:35 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -0,0 +1,36 @@
+package com.example.android.politicalpreparedness.election
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.android.politicalpreparedness.R
+import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
+
+class VoterInfoFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        //TODO: Add ViewModel values and create ViewModel
+        val binding = DataBindingUtil.inflate<FragmentVoterInfoBinding>(inflater, R.layout.fragment_voter_info, container, false)
+
+        //TODO: Add binding values
+
+        //TODO: Populate voter info -- hide views without provided data.
+        /**
+        Hint: You will need to ensure proper data is provided from previous fragment.
+        */
+
+
+        //TODO: Handle loading of URLs
+
+        //TODO: Handle save button UI state
+        //TODO: cont'd Handle save button clicks
+        return binding.root
+    }
+
+    //TODO: Create method to load URL intents
+
+}
